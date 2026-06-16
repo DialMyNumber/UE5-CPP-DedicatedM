857976af904502c2f46173f3aa5ba89374ee5222
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-06-16 19:13:27 +0900

    PlayerCharacter 내용 구현 - IA, IMC

--- Source/DedicatedM/Character/DMPlayerCharacter.cpp
+++ Source/DedicatedM/Character/DMPlayerCharacter.cpp
@@ -2,15 +2,51 @@
 
 #include "DMPlayerCharacter.h"
 
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "EnhancedInputComponent.h"
+
 ADMPlayerCharacter::ADMPlayerCharacter()
 {
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	GetCharacterMovement()->bUseControllerDesiredRotation = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f);
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->TargetArmLength = 500.f;
+	SpringArm->bUsePawnControlRotation = true;
+	SpringArm->SetupAttachment(GetRootComponent());
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->bUsePawnControlRotation = false;
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
 
 }
 
 void ADMPlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (IsLocallyControlled() == true)	// Local PlayerController일 때
+	{
+		APlayerController* PC = Cast<APlayerController>(GetController());
+		checkf(IsValid(PC) == true, TEXT("PlayerController is invalid."));
+
+		// SubSystem : 싱글톤같은 객체, 싱글톤은 프로그램 전체에 걸쳐있기 때문에 불필요하게 LifeCycle이 오래 남음
+		// LocalPlayerSubSystem : LocalPlayer객체와 LifeCycle이 같음
+		UEnhancedInputLocalPlayerSubsystem* EILPS = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer());
+		checkf(IsValid(EILPS) == true, TEXT("EnhancedInputLocalPlayerSubsystem is invalid."));
+
+		EILPS->AddMappingContext(InputMappingContext, 0);
+	}
 	
 }
 
@@ -24,4 +60,47 @@ void ADMPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputC
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	UEnhancedInputComponent* EIC = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+
+	EIC->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::HandleMoveInput);
+
+	EIC->BindAction(LookAction, ETriggerEvent::Triggered, this, &ThisClass::HandleLookInput);
+
+	EIC->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
+	EIC->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+
+}
+
+void ADMPlayerCharacter::HandleMoveInput(const FInputActionValue& InValue)
+{
+	if (IsValid(Controller) == false)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Controller is invalid."));
+		return;
+	}
+
+	const FVector2D InMovementVector = InValue.Get<FVector2D>();
+
+	const FRotator ControlRotation = Controller->GetControlRotation();
+	const FRotator ControlYawRotation(0.0f, ControlRotation.Yaw, 0.0f);
+
+	const FVector ForwardDirection = FRotationMatrix(ControlYawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(ControlYawRotation).GetUnitAxis(EAxis::Y);
+
+	AddMovementInput(ForwardDirection, InMovementVector.X);
+	AddMovementInput(RightDirection, InMovementVector.Y);
+}
+
+void ADMPlayerCharacter::HandleLookInput(const FInputActionValue& InValue)
+{
+	if (IsValid(Controller) == false)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Controller is invalid."));
+		return;
+	}
+
+	const FVector2D InLookVector = InValue.Get<FVector2D>();
+
+	AddControllerYawInput(InLookVector.X);
+	AddControllerPitchInput(InLookVector.Y);
 }
--- Source/DedicatedM/Character/DMPlayerCharacter.h
+++ Source/DedicatedM/Character/DMPlayerCharacter.h
@@ -4,22 +4,68 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
 #include "DMPlayerCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class DEDICATEDM_API ADMPlayerCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+#pragma region ACharacter Override
+
 public:
 	ADMPlayerCharacter();
 
-protected:
 	virtual void BeginPlay() override;
 
-public:	
 	virtual void Tick(float DeltaTime) override;
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+#pragma endregion
+
+#pragma region DMPlayerCharacter Components
+
+public:
+	FORCEINLINE USpringArmComponent* GetSpringArm() const { return SpringArm; }
+
+	FORCEINLINE UCameraComponent* GetCamera() const { return Camera; }
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "DMPlayerCharacter|Components")
+	TObjectPtr<USpringArmComponent> SpringArm;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "DMPlayerCharacter|Components")
+	TObjectPtr<UCameraComponent> Camera;
+
+#pragma endregion
+
+#pragma region Input
+
+private:
+	void HandleMoveInput(const FInputActionValue& InValue);
+
+	void HandleLookInput(const FInputActionValue& InValue);
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "DMPlayerCharacter|Input")
+	TObjectPtr<UInputMappingContext> InputMappingContext;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "DMPlayerCharacter|Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "DMPlayerCharacter|Input")
+	TObjectPtr<UInputAction> LookAction;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "DMPlayerCharacter|Input")
+	TObjectPtr<UInputAction> JumpAction;
+
+#pragma endregion
 
 };
