645ea2d6eff2e2043816be076ab1824b6ff8c53b
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-06-17 19:52:08 +0900

    NetMode를 알아내는 매크로, 현재 함수의 위치를 알려주는 매크로, 앞 두개의 매크로를 이용하여 NetMode와 함수 위치를 포함한 UE_LOG 매크로 정의

--- Source/DedicatedM/DedicatedM.cpp
+++ Source/DedicatedM/DedicatedM.cpp
@@ -1,6 +1,12 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Dedicated.cpp
 
 #include "DedicatedM.h"
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DedicatedM, "DedicatedM" );
+
+#pragma region NetLogging
+
+DEFINE_LOG_CATEGORY(LogDMNet);
+
+#pragma endregion
