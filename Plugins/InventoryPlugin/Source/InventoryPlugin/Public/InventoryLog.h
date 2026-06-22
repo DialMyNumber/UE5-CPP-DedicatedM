23826a72d013ff868bf069d0f2e7fbb0ffab974e
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-06-22 15:05:47 +0900

    로그 형식 변경

--- Plugins/InventoryPlugin/Source/InventoryPlugin/Public/InventoryLog.h
+++ Plugins/InventoryPlugin/Source/InventoryPlugin/Public/InventoryLog.h
@@ -4,12 +4,31 @@
 
 #include "CoreMinimal.h"
 
-#pragma region NetLogging
-
 INVENTORYPLUGIN_API DECLARE_LOG_CATEGORY_EXTERN(InventoryLog, Log, All);
 
-#define NETMODE_TCHAR ((GetNetMode() == ENetMode::NM_Client) ? *FString::Printf(TEXT("Client%02d"), UE::GetPlayInEditorID()) : ((GetNetMode() == ENetMode::NM_Standalone) ? TEXT("StandAlone") : TEXT("Server")))
-#define FUNCTION_TCHAR (ANSI_TO_TCHAR(__FUNCTION__))
-#define INVEN_NET_LOG(LogCategory, Verbosity, Format, ...) UE_LOG(LogCategory, Verbosity, TEXT("[%s] %s | %s"), NETMODE_TCHAR, FUNCTION_TCHAR, *FString::Printf(Format, ##__VA_ARGS__))
+#define NETMODE_TCHAR \
+((GetNetMode() == ENetMode::NM_Client) ? \
+*FString::Printf(TEXT("Client%02d"), UE::GetPlayInEditorID()) : \
+((GetNetMode() == ENetMode::NM_Standalone) ? TEXT("Standalone") : TEXT("Server")))
 
-#pragma endregion
+#define AUTH_TCHAR(Actor) \
+((Actor && Actor->HasAuthority()) ? TEXT("Authority") : TEXT("Remote"))
+
+#define ROLE_TCHAR(Actor) \
+((Actor) ? \
+*StaticEnum<ENetRole>()->GetNameStringByValue((int64)(Actor)->GetLocalRole()).RightChop(5) \
+: TEXT("NoActor"))
+
+#define FUNCTION_TCHAR (TEXT(__FUNCTION__))
+
+#define INVEN_NET_LOG(LogCategory, Verbosity, Actor, Format, ...) \
+UE_LOG(LogCategory, Verbosity, TEXT("[%s][%s][%s] %s | ") TEXT(Format), \
+	NETMODE_TCHAR, \
+	AUTH_TCHAR(Actor), \
+	ROLE_TCHAR(Actor), \
+	FUNCTION_TCHAR, \
+	##__VA_ARGS__)
+
+/*			Usage
+INVEN_NET_LOG(InventoryLog, Warning, GetOwner(), "BeginPlay");
+*/
