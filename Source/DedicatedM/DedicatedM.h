9cf33b57f53dae31eede439fe090d7a39e8345cc
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-06-22 15:05:21 +0900

    간단한 구분 기호 추가

--- Source/DedicatedM/DedicatedM.h
+++ Source/DedicatedM/DedicatedM.h
@@ -10,6 +10,6 @@ DEDICATEDM_API DECLARE_LOG_CATEGORY_EXTERN(LogDMNet, Log, All);
 
 #define NETMODE_TCHAR ((GetNetMode() == ENetMode::NM_Client) ? *FString::Printf(TEXT("Client%02d"), UE::GetPlayInEditorID()) : ((GetNetMode() == ENetMode::NM_Standalone) ? TEXT("StandAlone") : TEXT("Server")))
 #define FUNCTION_TCHAR (ANSI_TO_TCHAR(__FUNCTION__))
-#define DM_LOG_NET(LogCategory, Verbosity, Format, ...) UE_LOG(LogCategory, Verbosity, TEXT("[%s] %s %s"), NETMODE_TCHAR, FUNCTION_TCHAR, *FString::Printf(Format, ##__VA_ARGS__))
+#define DM_LOG_NET(LogCategory, Verbosity, Format, ...) UE_LOG(LogCategory, Verbosity, TEXT("[%s] %s | %s"), NETMODE_TCHAR, FUNCTION_TCHAR, *FString::Printf(Format, ##__VA_ARGS__))
 
 #pragma endregion
